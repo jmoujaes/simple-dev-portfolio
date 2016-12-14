d27fd5fe14f580fb59cc3a03155fe699a23198fe
Jason Moujaes <jasonmou@yahoo.com>
2016-12-13 20:18:15 -0800

    Add README. Add more configurable variables.

---
+++ README.md
@@ -0,0 +1,5 @@
+This project is meant to be a super simple site that developers and others can use to showcase themselves and their work. It is html/css/javascript and depends on bootstrap 4 and fontawesome. 
+
+Take a look at config.js to see which variables are configurable.
+
+Extend the site to fit your needs by adding more cards. https://v4-alpha.getbootstrap.com/components/card/ 
--- config.js
+++ config.js
@@ -1,10 +1,11 @@
 var config = {
   'name': 'Jason Moujaes',
   'backgroundcolor': 'white',
+  'bio': 'A description from the config.js file that says a little something about myself. If left empty, this should be blank.',
   'facebook': '#',
-  'github': '#',
+  'github': 'https://github.com/rebbuz',
   'linkedin': '#',
   'profilepic': 'https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/3/005/0af/199/3e48a04.jpg',
   'twitter': '#',
-  'projectsite': '#',
+  'projectsite': 'https://github.com/rebbuz/simple-dev-portfolio',
 };
