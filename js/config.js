8c46128debc09508cbd5926d889722d150f28d70
Jason Moujaes <jasonmou@yahoo.com>
2016-12-13 20:42:06 -0800

    add linkedin url and move config file to the js folder since it is a js file.

--- js/config.js
+++ js/config.js
@@ -4,7 +4,7 @@ var config = {
   'bio': 'A description from the config.js file that says a little something about myself. If left empty, this should be blank.',
   'facebook': '#',
   'github': 'https://github.com/rebbuz',
-  'linkedin': '#',
+  'linkedin': 'https://www.linkedin.com/in/jmoujaes',
   'profilepic': 'https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/3/005/0af/199/3e48a04.jpg',
   'twitter': '#',
   'projectsite': 'https://github.com/rebbuz/simple-dev-portfolio',
