264a9fb835ba158bda9b3859310b6b3e6f9fdb71
Jason Moujaes <jasonmou@yahoo.com>
2016-12-13 19:32:58 -0800

    initial design

---
+++ config.js
@@ -0,0 +1,10 @@
+var config = {
+  'name': 'Jason Moujaes',
+  'backgroundcolor': 'white',
+  'facebook': '#',
+  'github': '#',
+  'linkedin': '#',
+  'profilepic': 'https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/3/005/0af/199/3e48a04.jpg',
+  'twitter': '#',
+  'projectsite': '#',
+};
