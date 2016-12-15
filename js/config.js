008d83deeea4a8c1dc2e8dc06bac85cfdafb0699
Jason Moujaes <jasonmou@yahoo.com>
2016-12-14 16:55:32 -0800

    make default config values generic

--- js/config.js
+++ js/config.js
@@ -1,12 +1,12 @@
 var config = {
-  'name': 'Jason Moujaes',
+  'name': 'John Doesmith',
   'backgroundcolor': 'white',
   'bio': 'A description from the config.js file that says a little something about myself. If left empty, this should be blank.',
   'facebook': '#',
-  'github': 'https://github.com/rebbuz',
-  'linkedin': 'https://www.linkedin.com/in/jmoujaes',
-  'profilepic': 'https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/3/005/0af/199/3e48a04.jpg',
-  'resume': 'https://s3.amazonaws.com/jasonmoujaes.com/jmoujaes_resume.pdf',
+  'github': '#',
+  'linkedin': '#',
+  'profilepic': '#',
+  'resume': '#',
   'twitter': '#',
   'projectsite': 'https://github.com/rebbuz/simple-dev-portfolio',
 };
