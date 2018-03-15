029d890b760b27e9722467a35b6ba62d2040ffa0
Jason Moujaes <jmoujaes@gmail.com>
2018-03-15 07:23:33 -0700

    updated bootstrap4 beta to bootstrap 4, adjusted HTML to deal with changes; added email config option

--- js/config.js
+++ js/config.js
@@ -1,7 +1,8 @@
 var config = {
-  'name': 'John Smith',
+  'name': 'Jane Doe',
   'backgroundcolor': 'white',
   'bio': 'A description from the config.js file that says a little something about you. If left empty, this will be blank.',
+  'email': 'your@email.com',
   'facebook': 'https://www.facebook.com/',
   'github': 'https://www.github.com/',
   'linkedin': 'https://www.linkedin.com/',
