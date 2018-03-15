005ac864c1ea247aab617972f94a0036b9259d13
Jason Moujaes <jmoujaes@gmail.com>
2018-03-15 07:45:16 -0700

    make email more email-like

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ var config = {
   'name': 'Jane Doe',
   'backgroundcolor': 'white',
   'bio': 'A description from the config.js file that says a little something about you. If left empty, this will be blank.',
-  'email': 'your@email.com',
+  'email': 'my@email.com',
   'facebook': 'https://www.facebook.com/',
   'github': 'https://www.github.com/',
   'linkedin': 'https://www.linkedin.com/',
